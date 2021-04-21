5deae971564287ae75ad562ce27360b2c21e37f4
Paweł Trumiński <pawel.truminski@gmail.com>
2021-04-21 20:38:22 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using Communication;
+using System;
+using System.Threading.Tasks;
+
+namespace SignalRCoreClientTest
+{
+	class Program
+	{
+		static async Task Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			LiveService tmp = new LiveService();
+			await tmp.Send();
+		}
+	}
+}
